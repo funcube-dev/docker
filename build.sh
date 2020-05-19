a4d1bca61779b37047f4ce6359c658a1931e248a
funcube-dev <65111763+funcube-dev@users.noreply.github.com>
2020-05-19 19:50:00 +0100

    uncomment build container build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo Using path: $CURRENT_DIR
 TAG=$(cat $CURRENT_DIR/current_tag)
 echo Tagging with: $TAG
 
-#docker build --tag fcbuild:$TAG --file ./fcbuild.Dockerfile ./base-context
+docker build --tag fcbuild:$TAG --file ./fcbuild.Dockerfile ./base-context
 
 docker build --build-arg FROM_TAG=$TAG  --tag fcdecode:$TAG    --file ./decode.Dockerfile    ./empty-context
 docker build --build-arg FROM_TAG=$TAG  --tag fcencode:$TAG    --file ./encode.Dockerfile    ./empty-context
